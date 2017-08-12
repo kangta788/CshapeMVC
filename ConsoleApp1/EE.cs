c0664808b3cb0e40db639bd06528c473591c40b7
kangta788 <kangta788@yahoo.com.tw>
2017-08-12 15:58:28 +0800

    第三章練習重點:新的字串串接語法 重新練習列舉，時間轉換(ToShorttimestring)

---
+++ ConsoleApp1/EE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum company
+    {
+        微軟=1,
+        Google=2,
+        特斯拉=3,
+        FaceBook=4,
+        亞馬遜=5
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public enum Gender
+        {
+            男,女,男中女,女中男,非男女
+                //也可以不宣告數字 會自動排序
+        }
+
+        static void Main(string[] args)
+        {
+
+            //資料型別轉換 加入新的字串串接方法(好用不是?)
+            string inputString;
+            inputString = "2018/8/12 08:24:55";
+            Console.WriteLine($"inputString= {inputString}");
+
+            DateTime myDate;
+            myDate = Convert.ToDateTime(inputString);
+
+            Console.WriteLine($"一般時間顯示方式: {myDate.ToString()}");
+            Console.WriteLine($"簡短時間顯示方式: {myDate.ToShortDateString()}");
+
+
+            //再次練習列舉 要常用
+
+            company myConpany = company.亞馬遜;
+            Console.WriteLine($"我的公司是{myConpany}");
+
+
+            //注意軒個列舉的位置
+            Gender mygender = Gender.女中男;
+            Console.WriteLine($"我的性別是{mygender}");
+
+
+
+        }
+    }
+}
